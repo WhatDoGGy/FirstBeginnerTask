c69522b4d849f355d782d9cc5baf452730b54556
Oleksandr Skibin <oleksandr.skibin@nure.ua>
2021-01-26 15:36:14 +0300

    First commit

---
+++ Script.js
@@ -0,0 +1,11 @@
+var counter = 0;
+function Increase() {
+    counter++;
+    document.getElementById("counter").innerHTML = counter;
+  }
+  function Decrease() {
+    counter--;
+    document.getElementById("counter").innerHTML = counter;
+  }
+
+  
